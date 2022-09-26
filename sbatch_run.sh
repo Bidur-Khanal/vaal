38564806c4009c175081e894cce366e0e9355f6c
Bidur-Khanal <Bidur.Khanal@outlook.com>
2022-09-26 19:27:49 -0400

    changes to RC

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -4,14 +4,16 @@
 #SBATCH --account airetreat22 --partition tier3
 #SBATCH -n 1
 #SBATCH -c 8
-#SBATCH --gres=gpu:p4:1
+#SBATCH --gres=gpu:a100:1
 #SBATCH --mem=24g
 module purge
 conda activate dplearning
 Scale=$"0,0"
 Resize=$"256"
-Dataset=$"liver-seg"
-srun -n 1 python3 main.py --scale $Scale --resize $Resize --expt $EXPT --dataset $Dataset --method $METHOD --adversary_param $ADVER_PARAM --random_sampling_seed $RAND_SAM_SEED --mse_gamma1 $MSE_GAMMA --num_vae_steps $NUM_VAE --query_train_epochs 30
+# Dataset=$"liver-seg"
+# srun -n 1 python3 main.py --scale $Scale --resize $Resize --expt $EXPT --dataset $Dataset --method $METHOD --adversary_param $ADVER_PARAM --random_sampling_seed $RAND_SAM_SEED --mse_gamma1 $MSE_GAMMA --num_vae_steps $NUM_VAE --query_train_epochs 30
+Dataset=$"liver-seg-small"
+srun -n 1 python3 main.py --scale $Scale --resize $Resize --expt $EXPT --dataset $Dataset --method $METHOD --adversary_param $ADVER_PARAM --random_sampling_seed $RAND_SAM_SEED --mse_gamma1 $MSE_GAMMA --num_vae_steps $NUM_VAE --query_train_epochs 100
 
 # echo "$EXPT"
 # echo "$METHOD"
