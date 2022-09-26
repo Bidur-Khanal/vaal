ae2f44a0296b9266f1a794742b36ae2f8ddc9867
Bidur-Khanal <Bidur.Khanal@outlook.com>
2022-09-26 16:48:17 -0400

    fix argument

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -11,7 +11,7 @@ conda activate dplearning
 Scale=$"0,0"
 Resize=$"256"
 Dataset=$"liver-seg-small"
-srun -n 1 python3 main.py --scale $Scale --resize $Resize --expt $EXPT --dataset $Dataset --method $METHOD --adversary_param $ADVER_PARAM --random_sampling_seed $RAND_SAM_SEED --mmse_gamma1 $MSE_GAMMA --num_vae_steps $NUM_VAE
+srun -n 1 python3 main.py --scale $Scale --resize $Resize --expt $EXPT --dataset $Dataset --method $METHOD --adversary_param $ADVER_PARAM --random_sampling_seed $RAND_SAM_SEED --mse_gamma1 $MSE_GAMMA --num_vae_steps $NUM_VAE
 
 # echo "$EXPT"
 # echo "$METHOD"
