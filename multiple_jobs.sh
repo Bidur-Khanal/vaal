5a7a67a65cf44aca7b44f6f6a7040bfd6753b95a
Bidur-Khanal <Bidur.Khanal@outlook.com>
2022-09-26 18:40:36 -0400

    change the full dataset setting

--- multiple_jobs.sh
+++ multiple_jobs.sh
@@ -1,14 +1,14 @@
 #! bin bash -l
 
 job_File="sbatch_run.sh" 
-dir="sbatch_log"
+dir="sbatch_log/full_dataset_liver_seg"
 for adversary_param in  1 10 25
 do 
     for mse_gamma in 0.1 0.2 0.5 1
     do 
         for num_vae_steps in 1 2 3
         do 
-            EXPT=EXPT_MULTI_VAAL_adver"$adversary_param"_mse"$mse_gamma"_num_vae_steps"$num_vae_steps"
+            EXPT=Full_dataset_liver_seg_EXPT_MULTI_VAAL_adver"$adversary_param"_mse"$mse_gamma"_num_vae_steps"$num_vae_steps"
             STD=$dir/STD_MULTI_VAAL_adver"$adversary_param"_mse"$mse_gamma"_num_vae_steps"$num_vae_steps".out
             ERR=$dir/ERR_MULTI_VAAL_adver"$adversary_param"_mse"$mse_gamma"_num_vae_steps"$num_vae_steps".err
             METHOD="multimodal_VAAL"
@@ -24,7 +24,7 @@ do
             export NUM_VAE;
             export RAND_SAM_SEED;
 
-            sbatch -J $EXPT -o $STD -t 03-05:00:00 -e $ERR $job_File
+            sbatch -J $EXPT -o $STD -t 05-00:00:00 -e $ERR $job_File
            
         done;
     done;
