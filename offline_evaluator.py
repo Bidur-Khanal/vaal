1389871208c560e8d38ad686fcd1ce6d6834a2cf
Bidur-Khanal <Bidur.Khanal@outlook.com>
2022-09-29 02:54:28 -0400

    add offline evaluator per class dice

--- offline_evaluator.py
+++ offline_evaluator.py
@@ -1,26 +1,25 @@
+import logging
 from multimodal_VAAL_solver import multi_modal_VAAL_Solver
 import torch
 from torchvision import datasets, transforms
 import torch.utils.data.sampler  as sampler
 import torch.utils.data as data
-
+import torch.nn as nn
 import numpy as np
-import argparse
 import random
-import os
-
+from pathlib import Path
 from custom_datasets import *
 import model
 import multi_modal_model
-import vgg
 from VAAL_solver import VAAL_Solver
 from utils import *
 import arguments
-from evaluate import evaluate
+from evaluate import evaluate, evaluate_classwise
 from unet import UNet
-from task_solver import train_task
 import wandb
 import torch.backends.cudnn as cudnn
+import warnings
+warnings.filterwarnings("ignore")
 
 ## Set Seed
 def fix_seed(seed):
@@ -37,21 +36,8 @@ def fix_seed(seed):
 def main(args):
 
     # (Initialize logging)
-    experiment = wandb.init(project='U-Net-active-learning-final-RC2')
+    experiment = wandb.init(project='U-Net-active-learning-final-RC-eval')
     
-    # if args.dataset == 'cifar10':
-    #     test_dataloader = data.DataLoader(
-    #             datasets.CIFAR10(args.data_path, download=True, transform=cifar_transformer(), train=False),
-    #         batch_size=args.batch_size, drop_last=False)
-
-    #     train_dataset = CIFAR10(args.data_path)
-
-    #     args.num_images = 50000
-    #     args.num_val = 5000
-    #     args.budget = 2500
-    #     args.initial_budget = 5000
-    #     args.num_classes = 10
-
     if args.dataset == 'liver-seg':
         test_dataloader = data.DataLoader(
                 datasets.ImageFolder(args.data_path, transform=imagenet_transformer()),
@@ -121,17 +107,13 @@ def main(args):
  
             
     args.device = torch.device('cuda:'+args.gpu_id if torch.cuda.is_available() else 'cpu')
+    args.evaluation_only = True
    
 
     splits = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4]
-    
-    current_indices = list(initial_indices)
     for i, split in enumerate(splits):
 
 
-        # need to retrain all the models on the new images
-        # re initialize and retrain the models
-        # task_model = vgg.vgg16_bn(num_classes=args.num_classes)
         experiment.log({
                     'split': split,
         
@@ -141,69 +123,30 @@ def main(args):
 
         task_model = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
         task_model.to(device=args.device)
-        train_task(args, net=task_model, train_loader = querry_dataloader, val_loader = val_dataloader, test_loader= test_dataloader,
-                  epochs=args.epochs,
-                  batch_size=args.batch_size,
-                  learning_rate=args.lr,
-                  amp=args.amp, wandb_log= experiment, split = split)
+        test_task(args, net=task_model, train_loader = querry_dataloader, val_loader = val_dataloader, test_loader= test_dataloader,
+                   wandb_log= experiment, split = split)
 
-
-        ## all unlabeled train samples
-        unlabeled_indices = np.setdiff1d(list(all_indices), current_indices)
-        unlabeled_sampler = data.sampler.SubsetRandomSampler(unlabeled_indices)
-        unlabeled_dataloader = data.DataLoader(train_dataset, 
-                sampler=unlabeled_sampler, batch_size=args.batch_size, drop_last=False)
 
         if split == splits[-1]:
             break
 
-        if args.method == 'VAAL':
-            #### initilaize the VAAL models
-            VAAL_solver = VAAL_Solver(args, test_dataloader)
-            vae = model.VAE(args.latent_dim)
-            discriminator = model.Discriminator(args.latent_dim)
 
-            vae = vae.to(device = args.device)
-            discriminator = discriminator.to(device = args.device)
-
-            # train the models on the current data
-            vae, discriminator = VAAL_solver.train(split,querry_dataloader,
-                                                val_dataloader,
-                                                vae, 
-                                                discriminator,
-                                                unlabeled_dataloader)
-            sampled_indices = VAAL_solver.sample_for_labeling(vae, discriminator, unlabeled_dataloader, unlabeled_indices)
+def test_task(args,net, train_loader, val_loader, test_loader,wandb_log = None, split = 1):
 
 
-        elif args.method == 'multimodal_VAAL':
-            
-            #### initilaize the VAAL models
-            multimodal_VAAL_solver = multi_modal_VAAL_Solver(args,test_dataloader)
-            vae = multi_modal_model.VAE(args.latent_dim)
-            discriminator = multi_modal_model.Discriminator(args.latent_dim)
+    scale = tuple(float(i) for i in args.scale.split(","))
+    if min(scale) == 0:
+        scale = None
 
-            vae = vae.to(device = args.device)
-            discriminator = discriminator.to(device = args.device)
-
-            # train the models on the current data
-            vae, discriminator = multimodal_VAAL_solver.train(split,querry_dataloader,
-                                                val_dataloader,
-                                                vae, 
-                                                discriminator,
-                                                unlabeled_dataloader)
-            sampled_indices = multimodal_VAAL_solver.sample_for_labeling(vae, discriminator, unlabeled_dataloader, unlabeled_indices)
+    dir_checkpoint = Path('./checkpoints/')
+    val_score = evaluate_classwise(net, val_loader, args.device)
+    logging.info('Validation Dice score: {}'.format(val_score))
+    net.load_state_dict(torch.load(str(dir_checkpoint)+'/'+args.expt + '/'+ 'checkpoint'+str(split)+'.pth'))
+    test_score = evaluate_classwise(net, test_loader, args.device)
+    wandb_log.log({'test Dice': test_score})
 
 
-        elif args.method == "RandomSampling":
-            
-            random.seed(args.random_sampling_seed)
-            random.shuffle(unlabeled_indices)
-            sampled_indices = unlabeled_indices[:args.budget]
 
-        current_indices = list(current_indices) + list(sampled_indices)
-        sampler = data.sampler.SubsetRandomSampler(current_indices)
-        querry_dataloader = data.DataLoader(train_dataset, sampler=sampler, 
-                batch_size=args.batch_size, drop_last=True)
 
 if __name__ == '__main__':
     args = arguments.get_args()
