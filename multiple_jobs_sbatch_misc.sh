d78586e9c05262a371e9aaef2be929978f5c4b8d
Bidur-Khanal <Bidur.Khanal@outlook.com>
2022-10-11 14:56:34 -0400

    add full training script

---
+++ multiple_jobs_sbatch_misc.sh
@@ -0,0 +1,40 @@
+#! bin bash -l
+
+job_File="sbatch_misc.sh" 
+# dir="sbatch_log/small_dataset_liver_seg"
+# dir="sbatch_log/dataset_liver_seg_mse_2"
+# dir ="sbatch_log/dataset_liver_seg_gallbladder_filtered"
+dir="sbatch_log/dataset_liver_seg_gallbladder_removed_class_no_less_than_3"
+# dir="sbatch_log/dataset_liver_seg_gallbladder_filtered_10_percent_gap"
+
+# RAND_SAM_SEED=$"1000"
+# # EXPT=full_dataset_liver_seg_gallbladder_filtered_10_percent_gap_EXPT_RandomSampling_seed_"$RAND_SAM_SEED"
+# EXPT=full_dataset_liver_seg_gallbladder_filtered_EXPT_RandomSampling_seed_"$RAND_SAM_SEED"
+# STD=$dir/STD_RandomSampling_seed_"$RAND_SAM_SEED".out
+# ERR=$dir/ERR_RandomSampling_seed_"$RAND_SAM_SEED".err
+# METHOD="RandomSampling"
+# EXPT=full_dataset_liver_seg_gallbladder_filtered_10_percent_gap_EXPT_RandomSampling_seed_"$RAND_SAM_SEED"
+
+adversary_param=$"25"
+num_vae_steps=$"2"
+seed=$"0"
+
+
+EXPT=dataset_liver_seg_gallbladder_removed_class_no_less_than_3_EXPT_full_train_seed_"$seed"
+STD=$dir/STD_full_train_seed_"$seed".out
+ERR=$dir/ERR_full_train_seed_"$seed".err
+METHOD="RandomSampling"
+ADVER_PARAM=$adversary_param
+NUM_VAE=$num_vae_steps
+RAND_SAM_SEED=$seed
+SEED=$seed
+
+
+export EXPT;
+export METHOD;
+export ADVER_PARAM;
+export NUM_VAE;
+export RAND_SAM_SEED;
+export SEED;
+
+sbatch -J $EXPT -o $STD -t 00-15:00:00 -e $ERR $job_File
---
+++ sbatch_misc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+
+#SBATCH --account airetreat22 --partition tier3
+#SBATCH -n 1
+#SBATCH -c 8
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mem=24g
+module purge
+conda activate dplearning
+Scale=$"0,0"
+Resize=$"256"
+
+# Dataset=$"liver-seg-gallbladder-removed"
+# srun -n 1 python3 main.py --scale $Scale --resize $Resize --expt $EXPT --dataset $Dataset --method $METHOD --random_sampling_seed $RAND_SAM_SEED --query_train_epochs 30
+
+Dataset=$"liver-seg-gallbladder-removed-class-no-less-than-3"
+srun -n 1 python3 main.py --scale $Scale --resize $Resize --expt $EXPT --dataset $Dataset --method $METHOD --adversary_param $ADVER_PARAM --random_sampling_seed $RAND_SAM_SEED --num_vae_steps $NUM_VAE --seed $SEED --query_train_epochs 40 --train_full
+
+
