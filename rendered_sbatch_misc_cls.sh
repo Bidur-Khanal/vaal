6305e1a35877d9ea2a69ae9f8b8f341420c6745f
Bidur-Khanal <Bidur.Khanal@outlook.com>
2022-10-11 16:06:46 -0400

    minor fixes

--- rendered_sbatch_misc_cls.sh
+++ rendered_sbatch_misc_cls.sh
@@ -12,6 +12,6 @@ Scale=$"0,0"
 Resize=$"128"
 
 Dataset=$"rendered-classification-liver-seg-gallbladder-removed"
-srun -n 1 python3 main.py --scale $Scale --resize $Resize --expt $EXPT --dataset $Dataset --method $METHOD --adversary_param $ADVER_PARAM --random_sampling_seed $RAND_SAM_SEED --mse_gamma2 $MSE_GAMMA --num_vae_steps $NUM_VAE --seed $SEED --query_train_epochs 50 --task_type classification --train_full
+srun -n 1 python3 main.py --scale $Scale --resize $Resize --expt $EXPT --dataset $Dataset --method $METHOD --adversary_param $ADVER_PARAM --random_sampling_seed $RAND_SAM_SEED --num_vae_steps $NUM_VAE --seed $SEED --query_train_epochs 50 --task_type classification --train_full
 
 
